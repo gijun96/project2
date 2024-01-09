628da26a1c5c18ea0d174475d51d930f884695e6
gijun <gijun96.github.com>
2024-01-09 18:31:06 +0900

    Initial commit

---
+++ src/test/java/com/damoim/BedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.damoim;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
