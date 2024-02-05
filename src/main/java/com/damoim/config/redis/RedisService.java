3c23d1a4579db3bef242100453e9ec5a3eed1b5a
gijun <gijun96.github.com>
2024-02-05 16:49:30 +0900

    redis연동
    coolsms 연동중

---
+++ src/main/java/com/damoim/config/redis/RedisService.java
@@ -0,0 +1,74 @@
+package com.damoim.config.redis;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.core.HashOperations;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.Duration;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
+
+@Component
+@RequiredArgsConstructor
+public class RedisService {
+
+    private final RedisTemplate<String, Object> redisTemplate;
+
+
+//    setValue() : key와 data 를 Redis에 저장한다. 만약 데이터에 만료시간을 설정하고 싶다면
+//                  세번쨰 파라미터로 Duration 객체를 전달한다.
+//    getValues() : key를 파라미터로 받아 key를 기반으로 데이터를 조회한다.
+//    deleteValues(): key를 파라미터로 받아 key를 기반으로 데이터를 삭제한다.
+//    checkExistsValues(): 조회하려는 데이터가 없으면 false를 반환한다.
+    public void setValues(String key, String data){
+        ValueOperations<String, Object> values= redisTemplate.opsForValue();
+        values.set(key, data);
+    }
+
+    public void setValues(String key, String data, Duration duration){
+        ValueOperations<String, Object> values = redisTemplate.opsForValue();
+        values.set(key, data, duration);
+    }
+
+    @Transactional(readOnly = true)
+    public String getValues(String key){
+        ValueOperations<String, Object> values = redisTemplate.opsForValue();
+        if(values.get(key) == null){
+            return "false";
+        }
+        return (String)values.get(key);
+    }
+
+    public void deleteValues(String key){
+        redisTemplate.delete(key);
+    }
+
+    public void expireValues(String key, int timeout) {
+        redisTemplate.expire(key, timeout, TimeUnit.MILLISECONDS);
+    }
+
+    public void setHashOps(String key, Map<String, String> data) {
+        HashOperations<String, Object, Object> values = redisTemplate.opsForHash();
+        values.putAll(key, data);
+    }
+
+    @Transactional(readOnly = true)
+    public String getHashOps(String key, String hashKey) {
+        HashOperations<String, Object, Object> values = redisTemplate.opsForHash();
+        return Boolean.TRUE.equals(values.hasKey(key, hashKey)) ? (String) redisTemplate.opsForHash().get(key, hashKey) : "";
+    }
+
+    public void deleteHashOps(String key, String hashKey) {
+        HashOperations<String, Object, Object> values = redisTemplate.opsForHash();
+        values.delete(key, hashKey);
+    }
+
+    public boolean checkExistsValue(String value) {
+        return !value.equals("false");
+    }
+
+
+}
--- src/main/java/com/damoim/controller/SignupController.java
+++ src/main/java/com/damoim/controller/SignupController.java
@@ -5,12 +5,26 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-@RequiredArgsConstructor
 @RestController
+@RequiredArgsConstructor
 @RequestMapping("/api")
 public class SignupController {
 
     private final MemberService memberService;
+
+//    @Value("${coolsms.api.key}")
+//    String apiKey;
+//    @Value("${coolsms.api.APISecret}")
+//    String APISecret;
+//    @Autowired
+//    final DefaultMessageService messageService;
+//
+//    public SignupController(){
+//        this.messageService= NurigoApp.INSTANCE.initialize(apiKey, APISecret, "https://api.coolsms.co.kr");
+//    }
+//
+
+
 
     @GetMapping("/checkById")
     public ResponseEntity checkById(@RequestParam("userId") String userId){
@@ -22,5 +36,25 @@ public class SignupController {
         }
     return ResponseEntity.status(200).build();
     }
+
+//    /**
+//     * 단일 메시지 발송 예제
+//     */
+//    @PostMapping("/send-one")
+//    public SingleMessageSentResponse sendOne() {
+//        Message message = new Message();
+//        // 발신번호 및 수신번호는 반드시 01012345678 형태로 입력되어야 합니다.
+//        message.setFrom("01036745733");
+//        message.setTo("01036745733");
+//        message.setText("한글 45자, 영자 90자 이하 입력되면 자동으로 SMS타입의 메시지가 추가됩니다.");
+//
+//
+//        SingleMessageSentResponse response = this.messageService.sendOne(new SingleMessageSendingRequest(message));
+//        System.out.println(response);
+//
+//        return response;
+//    }
+
+
 
 }
