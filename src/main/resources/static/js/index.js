6e17fa06509df2a1b301d8109a9bd6c7438539e8
Junkai336 <gjwnssla2@gmail.com>
2024-02-12 11:38:27 +0900

    섹션3,섹션4 ok / 헤더 푸터 ing

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -1,13 +1,26 @@
 $(document).ready(function () {
 // 섹션2 슬라이드 정의
         var swiper = new Swiper(".meeting_container", {
-            slidesPerView: 3,
-            spaceBetween: 100,
+            // slidesPerView: 3,
+            spaceBetween: 50,
             loop: true,
             navigation: {
                 prevEl: ".swiper-button-prev",
                 nextEl: ".swiper-button-next",
             },
+            breakpoints: {
+                200: {
+                    slidesPerView: 1,
+                },
+                900: {
+                    slidesPerView: 2,
+                    spaceBetween: 50,
+                },
+                1300: {
+                    slidesPerView: 3,
+                    spaceBetween: 70,
+                }
+            }
         });
 
 // 섹션3 슬라이드 정의
